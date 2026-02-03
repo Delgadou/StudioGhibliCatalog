bda540c7b1396c464ea85862b0522329317b16ba
Henrique Delgado <delgado.ohenrique@gmail.com>
2026-02-03 20:54:25 -0300

    create project

---
+++ StudioGhibliCatalog/StudioGhibliCatalog/StudioGhibliCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudioGhibliCatalogApp.swift
+//  StudioGhibliCatalog
+//
+//  Created by Henrique Delgado on 03/02/26.
+//
+
+import SwiftUI
+
+@main
+struct StudioGhibliCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
