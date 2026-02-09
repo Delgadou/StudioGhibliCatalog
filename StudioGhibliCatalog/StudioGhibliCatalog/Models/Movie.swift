7d52bc8f009c9c5dd6ef82a75f76eb7d6bb07a7e
Henrique Delgado <delgado.ohenrique@gmail.com>
2026-02-09 17:17:02 -0300

    add new properties in movie model

--- StudioGhibliCatalog/StudioGhibliCatalog/Models/Movie.swift
+++ StudioGhibliCatalog/StudioGhibliCatalog/Models/Movie.swift
@@ -8,9 +8,13 @@
 struct Movie: Decodable, Identifiable, Hashable {
     let id: String
     let title: String
+    let original_title_romanised: String
     let image: String
+    let movie_banner: String
     let description: String
     let release_date: String
     let rt_score: String
     let running_time: String
+    let director: String
+    let producer: String
 }
