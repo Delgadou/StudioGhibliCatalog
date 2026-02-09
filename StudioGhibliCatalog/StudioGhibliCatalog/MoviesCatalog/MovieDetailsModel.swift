3955af88007a35b6ad7e424706bccff7a5bdeb07
Henrique Delgado <delgado.ohenrique@gmail.com>
2026-02-09 17:17:22 -0300

    implement new design for movieDetailView

--- StudioGhibliCatalog/StudioGhibliCatalog/MoviesCatalog/MovieDetailsModel.swift
+++ StudioGhibliCatalog/StudioGhibliCatalog/MoviesCatalog/MovieDetailsModel.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 import CasePaths
 
-//Investigar por que final class
 @Observable
 @MainActor
-final class MovieDetailsModel: Identifiable {
-    var movie: Movie
+class MovieDetailsModel: Identifiable {
+    // MARK: Properties
+    let movie: Movie
+    var isDescriptionExpanded = false
     var onClose: (() -> Void)?
+    var detailItem: [DetailItem] {
+        [
+            DetailItem(type: .duration, value: movie.running_time),
+            DetailItem(type: .rating, value: movie.rt_score),
+            DetailItem(type: .producer, value: movie.producer)
+        ]
+    }
+
+    enum DetailType {
+        case duration
+        case rating
+        case producer
+
+        var title: String {
+            switch self {
+            case .duration: return "Duration"
+            case .rating: return "Rating"
+            case .producer: return "Producer"
+            }
+        }
+    }
+    
+    struct DetailItem: Identifiable {
+        let id = UUID()
+        let type: DetailType
+        let value: String
+    }
 
     init(movie: Movie) {
         self.movie = movie
     }
 
+    // MARK: Methods
     func close() {
         onClose?()
     }
